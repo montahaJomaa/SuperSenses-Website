d86707f7d3da57593ac21dcb361fd915a5ba75d8
amine Amine <amine.hz.hz.98@gmail.com>
2022-08-02 12:07:38 +0100

    interface nos solution

--- src/components/NosSolution/NosSolutionPa.js
+++ src/components/NosSolution/NosSolutionPa.js
@@ -3,24 +3,94 @@ import img1 from '../images/Groupe 14672.png';
 import img2 from '../images/Carbo-arroser-terre.png';
 import img3 from '../images/Group14411.png';
 import img4 from '../images/Group14483.png';
+import kitelectronique from '../images/Groupe 19574.png';
+import platform from '../images/Groupe 19575.png';
+import appmobile from '../images/Groupe 19576.png';
+import mob from "../images/Groupe 14747.png";
 const NosSolutionPa = () => {
 
   return (
     <div className="container ">
-      <div class="row p-3">
-        
-        <h2 class="d-flex justify-content-center" id="TitleHodhod">Hodhod:Devenez acteurs de votre Communes </h2>
-        <p class="w-75  rounded p-5" id="NosSolutionParag1">
-          La ‘Hodhod’ est une solution destinée à lutter contre la pollution citadine et elle permet aux municipalités d’avoir un canal de communication interactive avec les citoyens en particulier et tout l’écosystème en général.
-          Ceci grâce à une Apps Mobile nommé ‘Hodhod’ inspiré de l’histoire du Huppe qui était un messager au prophète Soliman. L’application exploite tous les capteurs embarqué sur le Smartphone qui sera un outil de collecte des données sur terrain et remonter d’une manière efficace aux divers services de la municipalité.
-        </p>
+      <h1 className="text-center">Nos Solutions</h1>
+      <div className="row mt-5">
+        <div className="col">
+          <h1>Hodhod</h1>
+          <p>
+            Super senses fournit ses solutions innovantes à impact afin de résoudre
+            des problèmes environnemental et digitaliser les villes tunisiennes.
+            La ‘Hodhod’ est une solution destinée à lutter contre la pollution citadine
+            et elle permet aux municipalités d’avoir un canal de communication interactive avec
+            les citoyens en particulier et tout l'écosystème en général. Notre solution se compose de :
+          </p>
+        </div>
+        <div className="col">
+          <img src={video} alt="video" className="videoBorder" />
+        </div>
       </div>
-      
-      <div class="row">
-        <img id="NosSolutionVideo" class="w-50" src={video} />
+
+      <div className="row text-center">
+
+        <div className="col img-thumbnail shadow" id="compBorder1">
+
+          <img src={kitelectronique} className="kitelectronique text-center " />
+          <h6>Capteurs embarqués / kit électronique Collecte des datas.</h6>
+        </div>
+
+        <div className="col">
+
+        </div>
+
+        <div className="col img-thumbnail shadow" id="compBorder2">
+
+
+          <img src={platform} className="platform text-center" />
+          <h6>Plateforme intelligente interactive.</h6>
+
+        </div>
+
+        <div className="col">
+
+        </div>
+
+        <div className="col img-thumbnail shadow" id="compBorder3">
+
+
+          <img src={appmobile} className="appmobile text-center" />
+          <h6>Application mobile.</h6>
+
+
+        </div>
+
       </div>
-      <p class="p-5 d-flex justify-content-center">Super senses fournit ses solutions innovantes à impact afin de résoudre des problèmes environnemental et digitaliser les villes tunisiennes. </p>  
-     <h1 class="d-flex justify-content-center">Les Impacts du Hodhod</h1><br></br><br></br><br></br>
+
+      <div className="row " id="mobSize">
+        <div className="col">
+          <img src={mob} />
+        </div>
+        <div className="col">
+          <h6>L’application exploite tous les capteurs embarqué sur le Smartphone qui sera un outil de collecte des données sur terrain et remonter d’une manière efficace aux divers services de la municipalité.</h6>
+        </div>
+      </div>
+
+
+      <div className="row" id="backgroundimg">
+        <div className="row">
+          <h3 className="text-center" id="deveneztitle">Devenez acteurs de votre Communes avec HODHOD</h3>
+        </div>
+        <div className="row text-center" id="devenezParag">
+          <p>
+        Nous avons inventé Hodhod pour booster les municipalités à automatiser leurs procédures et engager les citoyens<br></br> 
+        pour une ville intelligente et moderne. La solution est destinée à lutter contre la pollution citadine et 
+        elle permet aux municipalités d’avoir un canal de communication interactive avec
+         les citoyens en particulier et tout l’écosystème en général. La collecte et la 
+         gestion des réclamations se fait instantanément d’une manière organisée.
+          La solution Hodhod est basée sur les nouvelles technologies principalement l’intelligence artificielle et 
+          l’internet des objets. L’application HodHod optimise les actions de la municipalité et
+         la mène vers un volet de digitalisation. Ses impacts sur la communauté sont nombreux.</p>
+        </div>
+
+      </div>
+      <h1 class="d-flex justify-content-center " id="ImpactBox">Les Impacts du Hodhod</h1><br></br><br></br><br></br>
       <div id="PicturesPadding">
       <div class="row p-3">
           <div class="col">
@@ -49,7 +119,10 @@ const NosSolutionPa = () => {
         </div>
       </div>
       </div>
+
     </div>
+
+
 
   );
 }
