ba642a689023abb216e47344a9fef6a8b1f71094
amine Amine <amine.hz.hz.98@gmail.com>
2022-08-04 12:41:30 +0100

    remake navfooter

--- clientSide/src/components/NavBarsHeaderFooter/NavBarFooter.js
+++ clientSide/src/components/NavBarsHeaderFooter/NavBarFooter.js
@@ -1,18 +1,53 @@
 import logo_supersenses from "../images/logo_supersenses_white copy.png";
-
+import logo_facebook from "../images/logo_facebook.png";
+import logo_instagram from "../images/logo_instagram.png";
+import logo_linkedIn from "../images/logo_linkedIn.png";
 const NavbarFooter = () => {
 
     return (
         <div className="navfooter">
             <div className="row">
                 <div className="col">
-
+                    <img src={logo_supersenses} className="w-50 " id="logo_sup" />
                 </div>
-                <div className="col">
+                <div className="col " id="col_nav">
+                    <div className="row">
+                        &nbsp;&nbsp; Espace carriere
+                    </div>
+                    <div className="row">
+                        &nbsp;&nbsp; Contactez nous
+                    </div>
+                    <div className="row">
+                        &nbsp;&nbsp; 92469629
 
+                    </div>
+                    <div className="row">
+                        &nbsp;&nbsp; contact@supersenses.tn
+                    </div>
+                    <div className="row">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="button">S'inscrire</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="col">
-
+                <div className="col" >
+                    <div className="row w-100" id="socialIcons">
+                        <div className="col">
+                            <h6 id="findOn">Find us on </h6>
+                        </div>
+                        <div className="col">
+                            <img src={logo_facebook} className="fb_logo" />
+                        </div>
+                        <div className="col">
+                            <img src={logo_linkedIn} className="linkedin_logo" />
+                        </div>
+                        <div className="col">
+                            <img src={logo_instagram} className="insta_logo" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
