4fa199bfe8c623fd4dadafed9d9fc24b65d96aef
amine Amine <amine.hz.hz.98@gmail.com>
2022-08-10 12:27:04 +0100

    update

--- clientSide/src/components/NavBarsHeaderFooter/NavBarHeader.jsx
+++ clientSide/src/components/NavBarsHeaderFooter/NavBarHeader.jsx
@@ -19,31 +19,21 @@ function NavBarHeader() {
     <div className="nav-header">
       <Navbar expand="lg"  >
         <Container>
-
           <Navbar.Brand href="/"><img src={logo_supersenses} alt="logo_supersenses" className="logo_supersenses" />
           </Navbar.Brand>
-
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-
             <Nav className="me-auto" >
-
-
-
               <LinkContainer to="/"><Nav.Link className='Nav_item' id='NavText_other'>Acceuil</Nav.Link></LinkContainer>
-
               <Dropdown as={ButtonGroup}>
                 {/* <Button href="/#boxs" variant="transparent">NOS SOLUTIONS</Button> */}
-                <Button variant='transparent' ><HashLink style={{textDecoration:"none", color:"black"}} smooth to="/#boxs">NOS SOLUTIONS</HashLink></Button>
-
+                <Button variant='transparent' ><HashLink style={{ textDecoration: "none", color: "black" }} smooth to="/#boxs">NOS SOLUTIONS</HashLink></Button>
                 <Dropdown.Toggle split variant="transparent" size="sm" id="dropdown-split-basic" />
                 <Dropdown.Menu size="sm">
                   <Dropdown.Item href="/Hodhod">Hodhod</Dropdown.Item>
                   <Dropdown.Item href="/SensesIA">SensesIA</Dropdown.Item>
-
                 </Dropdown.Menu>
               </Dropdown>
-
               <LinkContainer to="/NosExpertises"><Nav.Link className='Nav_item' id='NavText_other'>Notre expertise</Nav.Link></LinkContainer>
               <LinkContainer to="/Apropos"><Nav.Link className='Nav_item' id='NavText_other'>A propos</Nav.Link></LinkContainer>
               {/* <div className='dpSolution'><img src={language} id="languageIcon" /></div> */}
@@ -66,7 +56,6 @@ function NavBarHeader() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
       <ProgressBar
         id="ProgressBar"
         color="#25ACDE"
@@ -75,9 +64,7 @@ function NavBarHeader() {
         position="relative"
         gradient={true}
         gradientColor="#eee"
-
       />
-
     </div>
   );
 }
